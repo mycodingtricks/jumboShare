4d5cb93e39f2f2a0f879246d46af4f6920ee7cda
Shubham <kshubham52@gmail.com>
2015-11-20 17:58:37 +0530

    Update jumboShare.js

--- assets/js/jumboShare.js
+++ assets/js/jumboShare.js
@@ -21,7 +21,7 @@
                   "0"+
                "</div>"+
                "<div class='mct_jumboShare_buttons' id='jumboShare_buttons_"+settings.id+"'>"+
-                "<a target=_blank rel=nofollow href='https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(settings.url)+"&t="+encodeURI(settings.title)+"' class='jumboShare_btn facebook'>facebook</a>"+
+                "<a target=_blank rel=nofollow href='https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(settings.url)+"&t="+encodeURI(settings.text)+"' class='jumboShare_btn facebook'>facebook</a>"+
             "</div>"+
           "</div>";
           return code;
