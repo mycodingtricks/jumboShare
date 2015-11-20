c364be1f54a6fc0018531941e33150e65fe86623
Shubham <kshubham52@gmail.com>
2015-11-20 16:45:43 +0530

    Update jumboShare.js

--- assets/js/jumboShare.js
+++ assets/js/jumboShare.js
@@ -29,22 +29,22 @@
             if(n>=1000) return (n/1000).toFixed(1)+'K';
             return n;
         }
-        this.updateCounter = function(){
-          $("jumboShare_counter_"+settings.rand).text(convertNumber(settings.total)).fadeIn();
+        function updateCounter(){
+          $("#jumboShare_counter_"+settings.rand).text(convertNumber(settings.total)).fadeIn();
         }
         function getCount(){
             var $this = this;
             $.getJSON('http://cdn.api.twitter.com/1/urls/count.json?url='+settings.url+'&callback=?',function(d){
                 settings.total += d.count;
-                $this.updateCounter();
+                updateCounter();
             });
             $.getJSON('https://api.facebook.com/method/fql.query?format=json&query=SELECT+total_count+FROM+link_stat+WHERE+url+%3D+%27'+encodeURIComponent(settings.url)+'%27&callback=?',function(d){
                 settings.total = d[0].total_count;
-                $this.updateCounter();
+                updateCounter();
             });
             $.getJSON("https://www.linkedin.com/countserv/count/share?url="+settings.url+"&format=jsonp&callback=?", function(data) {
                 settings.total = data.count;
-                $this.updateCounter();
+                updateCounter();
             });
             var GooglePlusdata = {
                 "method":"pos.plusones.get",
@@ -69,7 +69,7 @@
                 data: JSON.stringify(GooglePlusdata),
                 success: function(r){
                     settings.total = r.result.metadata.globalCounts.count;
-                    $this.updateCounter();
+                    updateCounter();
                 }
             });
         }
